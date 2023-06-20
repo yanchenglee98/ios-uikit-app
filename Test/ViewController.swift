35b464aade2ff4976a7f4e58f7d2904406b62a36
leeyancheng <leeyancheng@bigo.sg>
2023-06-20 16:25:29 +0800

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Lee Yan Cheng on 20/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
